adf22ed25c87b1d214fa658874ab1ecde114f140
Minhaz Mahmud <minhaz.m2@gmail.com>
2014-04-07 18:50:22 -0400

    fixing db config

--- webroot/config.php
+++ webroot/config.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_host = "localhost";
-$mysql_database = "course_data"; //create the database called "comment_sys"
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_host = "www.minhazm.com";
+$mysql_database = "feedme"; //create the database called "comment_sys"
+$mysql_user = "team1";
+$mysql_password = "team1";
 
 mysql_connect($mysql_host,$mysql_user,$mysql_password);
 mysql_select_db($mysql_database); 
