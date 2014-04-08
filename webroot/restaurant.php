dc55d2c17e5043777e65a7e0342ef9d84a2db8cd
Minhaz Mahmud <minhazm@gmail.com>
2014-04-07 20:54:19 -0400

    Updating navbar in restaurant.php to include navbar.php

--- webroot/restaurant.php
+++ webroot/restaurant.php
@@ -43,36 +43,10 @@
     
     
 <!------------------------ Start of Navbar ------------------------>
-    <div class="navbar navbar-inner navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="">FeedMe</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href=""><span>Home</span></a></li>
-            <li><a href=""><span>About</span></a></li>
-            <li><a href=""><span>Contact</span></a></li>
-              
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">FeedMe Quick <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Most Active Communities</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Food Types</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Popular</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Random</a></li>
-                </ul>
-            </li>
-          </ul>
-            
-          <a href="" class="pull-right navbar-text"><span>Login</span></a>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-    <!------------------------ End of Navbar ------------------------> 
+    <?php include_once('navbar.php') ?>
+
+  
+<!------------------------ End of Navbar ------------------------> 
     
     <!---Modal ---->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
