e32c1b2f50a3b76c8d17377737333d21d3298ff0
Kiante Brantley <kiantebrantley@kiantemacbook.home>
2014-04-28 23:28:17 -0400

    Created a php page to display search results, for searching for a user

---
+++ webroot/searchUserResults.php
@@ -0,0 +1,102 @@
+<!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+ <!-- FeedME CSS -->
+    <link href="./css/feedme_usersearch.css" rel="stylesheet" type="text/css">
+
+        <div class="container">
+            <div class="row">
+                
+              <div class="col-xs-12 col-md-6">
+                <div class="thumbnail clearfix">  
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="wrapper ">
+                             <img src="./img/profile.png" class="img-responsive imageClip pull-left span2 clearfix" style='margin-right:10px; width:auto;
+    max-height: 160;
+    overflow:hidden'>
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="caption" class="pull-left">
+                                <h4>      
+                                    <a href="#" >Luis Felipe Kaufmann</a>
+                                </h4>
+                                <medium class="hidden-xs"><b>Username: </b>Test test</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Gender: </b>Male</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Year Arrived: </b>1991</medium><br class="hidden-xs" />
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+                
+            <div class="col-xs-12 col-md-6">
+                <div class="thumbnail clearfix">  
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="wrapper ">
+                            <img src="./img/profile.png" class="img-responsive imageClip"  class="pull-left span2 clearfix" style='margin-right:10px'>
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="caption" class="pull-left">
+                                <h4>      
+                                    <a href="#" >Luis Felipe Kaufmann</a>
+                                </h4>
+                                <medium class="hidden-xs"><b>Username: </b>Test test</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Gender: </b>Male</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Year Arrived: </b>1991</medium><br class="hidden-xs" />
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+                
+            <div class="col-xs-12 col-md-6">
+                <div class="thumbnail clearfix">  
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="wrapper ">
+                            <img  src="./img/profile.png" class="img-responsive imageClip"  class="pull-left span2 clearfix" style='margin-right:10px'>
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="caption" class="pull-left">
+                                <h4>      
+                                    <a href="#" >Luis Felipe Kaufmann</a>
+                                </h4>
+                                <medium class="hidden-xs"><b>Username: </b>Test test</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Gender: </b>Male</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Year Arrived: </b>1991</medium><br class="hidden-xs" />
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+                
+            <div class="col-xs-12 col-md-6">
+                <div class="thumbnail clearfix">  
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="wrapper ">
+                            <img  src="./img/profile.png" class="img-responsive imageClip"  class="pull-left span2 clearfix" style='margin-right:10px'>
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="caption" class="pull-left">
+                                <h4>      
+                                    <a href="#" >Luis Felipe Kaufmann</a>
+                                </h4>
+                                <medium class="hidden-xs"><b>Username: </b>Test test</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Gender: </b>Male</medium><br class="hidden-xs" />
+                                <medium class="hidden-xs"><b>Year Arrived: </b>1991</medium><br class="hidden-xs" />
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+      
+        </div>
+    </div>
+    
