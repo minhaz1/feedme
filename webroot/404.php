22f69ef186ee669a5e2a1fe94e09412a208269a7
Kiante Brantley <kiante.brantley@gmail.com>
2014-04-30 00:53:21 -0400

    Updated the 404.php page

--- webroot/404.php
+++ webroot/404.php
@@ -15,24 +15,24 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="error-template" style="padding: 40px 15px;text-align: center;">
+            <div class="error-template" style="padding: 10px;text-align: center;">
+                <div class="error-actions" style="margin-top:20px;">
+                    <a style="width:40%;" href="index.php" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Take Me Home </a>
+                </div>
+                
                 <h1 style="font-size:70px;color:black">
                     Oops!</h1>
                 <h2 style="font-size:70px;color:black">
                     404 Not Found</h2><br><br>
                 <h2 class="error-details" style="font-size:30px;color:black">
-                    Sorry, AUDREY 2 has eaten the developer of the page you are requesting.
+                    Sorry, AUDREY 2 has eaten the developer <br> of the page you are requesting.
                 </h2>  
                 
                 <div class="container">
                     <div class="centered">
-                        <img class="img-responsiveimg-center" src="./img/feedme1.png">
+                        <img class="img-responsive img-center" src="./img/feedme1.png">
                     </div>
-                </div>
-                
-                <div class="error-actions" style="margin-top:50px;">
-                    <a style="width:80%;" href="index.php" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
-                        Take Me Home </a>
                 </div>
             </div>
         </div>
