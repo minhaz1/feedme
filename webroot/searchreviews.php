961808d0de2cfafed4f172d334582a684680376f
Minhaz Mahmud <minhaz.m2@gmail.com>
2014-05-05 01:26:07 -0400

    Fixing issue with search results

--- webroot/searchreviews.php
+++ webroot/searchreviews.php
@@ -75,7 +75,7 @@ $reviewid = "";
 
                        echo "</div><div class=\"col-xs-8 col-sm-4 col-md-6\" style=\"padding-left: 0px;\">
                             <h3 style=\"padding-top: 0px; margin-top:0px;\">
-                              <a href=\"restaurant.php?resid=6\" title=\"\">
+                              <a href=\"restaurant.php?resid=$resid\" title=\"\">
                                   $title
                               </a>
                             </h3>
